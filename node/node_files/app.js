15a2d516dd17f0fa0de6e4f6d2958ece71fb2310
Razzka Rusmantoro <razzkarusmantoro123@gmail.com>
2024-06-17 18:43:40 +0400

    organization

--- node/node_files/app.js
+++ node/node_files/app.js
@@ -4,9 +4,8 @@ const mysql = require('mysql');
 const path = require('path');
 const app = express();
 
-
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use(express.static(path.join(__dirname, '../../public')));
 
 const con = mysql.createConnection({
     host: "localhost",
@@ -15,24 +14,18 @@ const con = mysql.createConnection({
     database: "loginDB"
 });
 
-
 con.connect((err) => {
     if (err) throw err;
     console.log("Connected to DB");
 });
 
-
 app.get('/', (req, res) => {
-
-    res.sendFile(path.join(__dirname, '..', '..', 'login.html'));
+    res.sendFile(path.join(__dirname, '../../public/html/login.html'));
 });
-
 
 app.post('/register', (req, res) => {
-
     res.send('Registration done.');
 });
-
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
