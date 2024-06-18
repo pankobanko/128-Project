09e8c2f8d7e417d6df178e3b41377d9f1c5e11b9
Razzka Rusmantoro <razzkarusmantoro123@gmail.com>
2024-06-18 15:32:02 +0400

    it works!!!

--- node/node_files/app.js
+++ node/node_files/app.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const mysql = require('mysql');
 const path = require('path');
 const app = express();
+const encoder = bodyParser.urlencoded();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '../../public')));
@@ -27,8 +28,34 @@ app.get('/login', (req, res) => {
     res.sendFile(path.join(__dirname, '../../public/html/login.html'));
 });
 
+app.post("/login", function(req, res){
+    var username = res.body.username;
+    var password = res.body.password;
+    const login = `SELECT * FROM users WHERE username = ? and password = ?`;
+    con.query(login, [username, password], function(err, results, fields){
+        if (results.length > 0) {
+            res.redirect("/");
+        } else {
+            res.redirect("/login")
+        }
+        res.end();
+    })
+})
+
 app.post('/register', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../public/html/admin_home.html'));
+    const username = req.body.username;
+    const email = req.body.email;
+    const password = req.body.password;
+    const firstname = req.body.firstname;
+    const lastname = req.body.lastname;
+
+    const register = `INSERT INTO users (username, email, password, firstname, lastname) VALUES (?, ?, ?, ?, ?)`;
+    con.query(register, [username, email, password, firstname, lastname], (err, result) => {
+        if (err) throw err;
+
+        console.log("jordan be registered!");
+        res.sendFile(path.join(__dirname, '../../public/html/admin_home.html'));
+    });
 });
 
 const PORT = process.env.PORT || 3000;
