7349616859b679bf36c367dbe48dad730bd06b74
abdur2k58 <abdur2k58>
2024-06-20 18:48:38 +0400

    files

--- node/node_files/app.js
+++ node/node_files/app.js
@@ -7,6 +7,7 @@ const multer = require('multer');
 const app = express();
 const encoder = bodyParser.urlencoded();
 var fs = require("fs");
+var fm = require("../../public/recipes/function");
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -50,57 +51,6 @@ app.get('/login', (req, res) => {
     res.sendFile(path.join(__dirname, '../../public/html/login.html'));
 });
 
-const filename = "rec";
-var counter = 3;
-
-const code = ` // boilerplate
-<!DOCTYPE html>   
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Recipe Viewing</title>
-    <link rel="stylesheet" href="rec.css">
-</head>
-<body>
-    <div class="navbar">
-        <nav>    
-            <ul>
-                <li><a href="../home.html">Home</a></li>
-                <li><a href="../login.html">Log In</a></li>
-                <li><a href="#">About Us</a></li>
-                <li><a href="#">Contact</a></li>
-            </ul>
-        </nav>
-    </div>
-    <div class="page">
-        <div class="recipe-info">
-            <h1 id="recipe-title"></h1> <!--  food name/type -->
-            <p id="recipe-description"></p> <!-- description -->
-            <br><br>
-            <h2 id="information">Preparation details</h2> <!-- i.e cooking time, prep time  -->
-            <p id="recipe-details"></p> <!-- details to be entered here -->
-        </div>
-        <div class="ing">  <!-- ingredients list, i.e 200g cheese -->
-            <h2 id="sub1"></h2>
-            <p id="ing"></p>
-
-        </div>
-        <div class="instructs"> <!--  cooking instructions, i.e oven under 80c for 20 minutes / chop carrots until ... -->
-            <h2 id="sub2">Preparations and Instructions</h2>
-            <p id="inst"></p>
-        </div>
-        <img id="img"> ///// 
-    </div>
-    
-</body>
-</html>
-`
-function addRecipe(){                                                                   ///////////////////////////////////////////////////////////////////////////////////
-    fs.appendFile("rec" + (++counter) + ".html", code, function (err){
-        if (err) throw err;
-        console.log("new file successfully created with boilerplate attached.");
-    });
-}
 
 
 
@@ -169,7 +119,7 @@ app.post('/add-recipe', upload.single('recipe-image'), (req, res) => {
             console.error('There has been an error with uploading the recipe:', err);
             res.status(500).send('');
         } else {
-            addRecipe();
+            fm.addRecipe();
             res.redirect('/admin_home');
         }
     });
